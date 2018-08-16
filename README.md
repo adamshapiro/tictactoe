e58fa8c2fbd8c4cde2968bf0c5d724f92c867eee
adamshapiro <adam.shapiro595@gmail.com>
2018-08-16 12:25:34 -0400

    completed basic tic tac toe functionality

--- README.md
+++ README.md
@@ -1,24 +1,10 @@
 # README
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Simple app trying out some rudimentary React and Rails functionality.
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+To run:
+```
+bundle install
+rails s
+./bin/webpack-dev-server
+```
--- app/javascript/packs/board.jsx
+++ app/javascript/packs/board.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom'
 
 function Square(props) {
     return (
-        <div className='col-2 square border border-dark'>
-            <span onClick={props.onClick}>
-                {props.value}
-            </span>
+        <div className='col-4 col-sm-2 px-0 border border-dark'>
+            <div className='square'>
+                <span onClick={props.onClick}>
+                    {props.value}
+                </span>
+            </div>
         </div>
     )
 }
@@ -16,28 +18,78 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(3).fill( Array(3).fill(null) ),
-            xTurn: true
+            squares: Array(9).fill(null),
+            xTurn: true,
+            gameOver: false,
+            status: "Next player is X"
         };
     }
 
-    handleClick (row, col) {
+    handleClick (loc) {
         const squares = this.state.squares.slice();
-        if (squares[row][col])
+        if (squares[loc] || this.state.gameOver)
             return;
 
-        squares[row][col] = (this.state.xTurn ? 'X' : 'O');
+        squares[loc] = (this.state.xTurn ? 'X' : 'O');
+
+        this.calculateWinner(squares);
+    }
+
+    calculateWinner (squares) {
+        let gameOver, status;
+
+        for (let i = 0; i < 3; i++) {
+            let r = i*3;
+            if (
+                squares[r]
+                && (squares[r] == squares[r + 1] && squares[r] == squares[r + 2])
+            ) {
+                // check each row for a winner
+                gameOver = true;
+                status = "Winner: " + squares[r];
+                break;
+            } else if (
+                squares[i]
+                && (squares[i] == squares[3 + i] && squares[i] == squares[6 + i])
+            ) {
+                // then check each column
+                gameOver = true;
+                status = "Winner: " + squares[i];
+                break;
+            } else if (
+                squares[4]
+                && ((squares[4] == squares[0] && squares[4] == squares[8])
+                    || (squares[4] == squares[2] && squares[4] == squares[6]))
+            ) {
+                // then check diagonals
+                gameOver = true;
+                status = "Winner: " + squares[4];
+                break;
+            } else if (squares.indexOf(null) == -1) {
+                // if there is no winner but the board is filled, its a draw
+                gameOver = true;
+                status = "Draw";
+            } else {
+                // otherwise the game goes on
+                gameOver = false;
+                status = "Next player is " + (this.state.xTurn ? "O" : "X");
+            }
+        }
 
         this.setState({
             squares: squares,
-            xTurn: !this.state.xTurn
+            xTurn: !this.state.xTurn,
+            gameOver: gameOver,
+            status: status
         });
     }
 
     resetBoard () {
         this.setState({
-            squares: Array(3).fill( Array(3).fill(null) ),
-            xTurn: true
+            squares: Array(9).fill(null),
+            xTurn: true,
+            gameOver: false,
+            status: "Next player is X"
         });
     }
 
@@ -48,26 +100,27 @@ class Board extends React.Component {
             let row = [];
 
             for (let j = 0; j < 3; j++) {
+                let loc = i*3 + j;
                 row.push(
-                    <Square key={String(i) + String(j)}
-                        value={this.state.squares[i][j]}
-                        onClick={() => this.handleClick(i, j)} />
-                    )
+                    <Square key={'loc' + loc}
+                    value={this.state.squares[loc]}
+                    onClick={() => this.handleClick(loc)} />
+                )
             }
 
-            board.push(<div key={i} className='row board-row'>{row}</div>);
+            board.push(<div key={'row' + i} className='row'>{row}</div>);
         }
 
         return board;
     }
 
     render() {
-        const status = "Next player is " + (this.state.xTurn ? 'X' : 'O');
         return (
             <div className='container p-3'>
-                <div className='status'>{status}</div>
+                <h2>{this.state.status}</h2>
                 <div className='my-3'>{this.createBoard()}</div>
-                <button className='btn btn-primary' onClick={() => this.resetBoard()}>
+                <button className='btn btn-primary btn-lg'
+                    onClick={() => this.resetBoard()}>
                     Reset Board
                 </button>
             </div>
